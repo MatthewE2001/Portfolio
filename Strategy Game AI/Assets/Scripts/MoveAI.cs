4d36d3fb662e4b8c71447fe4ae4d8c1e0b28c264
Matt <matthew.esslie@mymail.champlain.edu>
2022-02-23 19:18:49 -0500

    working on making the ai move to some degree

--- Strategy Game AI/Assets/Scripts/MoveAI.cs
+++ Strategy Game AI/Assets/Scripts/MoveAI.cs
@@ -7,6 +7,10 @@ public class MoveAI : MonoBehaviour
     public Vector3 movePosition;
     public float moveSpeed;
 
+    public GameObject teamManager;
+
+    bool isMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,37 @@ public class MoveAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.transform.position == movePosition)
+        {
+            ChangeIsMoving();
+            teamManager.GetComponent<TeamManager>().SetLocationReached();
+        }
+
+        if (isMoving == true)
+        {
+            MoveTowardsLocation();
+        }
+    }
+
+    public void MoveTowardsLocation()
+    {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, movePosition, moveSpeed);
+    }
+
+    public void ChangeIsMoving()
+    {
+        if (isMoving == true)
+        {
+            isMoving = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
+    }
+
+    public void SetLocation(Vector3 location)
+    {
+        movePosition = location;
     }
 }
--- Strategy Game AI/Assets/Scripts/TeamManager.cs
+++ Strategy Game AI/Assets/Scripts/TeamManager.cs
@@ -19,23 +19,59 @@ public class TeamManager : MonoBehaviour
     public int startingFood;
     public int startingGold;
 
+    public GameObject goldObject;
+    public GameObject foodObject;
+
     int currentFood;
     int currentGold;
     int baseHealth; //might not need health of the base here i am not sure
     int currentUnitTrainingTime;
     int currentSoldierTrainingTime;
 
+    bool locationChangeNeeded = true;
+
     // Start is called before the first frame update
     void Start()
     {
         currentFood = startingFood;
         currentGold = startingGold;
+
+        SetAIMoveLocations();
+
+        for (int i = 0; i < aiUnits.Length; i++)
+        {
+            aiUnits[i].GetComponent<MoveAI>().ChangeIsMoving();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (locationChangeNeeded == true)
+        {
+            for (int i = 0; i < aiUnits.Length; i++)
+            {
+                if (aiUnits[i].transform.position == new Vector3(0,0,0))
+                {
+                    aiUnits[i].GetComponent<MoveAI>().SetLocation(gameObject.transform.position);
+                    aiUnits[i].GetComponent<MoveAI>().ChangeIsMoving();
+                }
+                
+                if (aiUnits[i].transform.position == goldObject.transform.position)
+                {
+                    aiUnits[i].GetComponent<MoveAI>().SetLocation(gameObject.transform.position);
+                    aiUnits[i].GetComponent<MoveAI>().ChangeIsMoving();
+                }
+                
+                if (aiUnits[i].transform.position == gameObject.transform.position)
+                {
+                    aiUnits[i].GetComponent<MoveAI>().SetLocation(goldObject.transform.position);
+                    aiUnits[i].GetComponent<MoveAI>().ChangeIsMoving();
+                }
+            }
+
+            locationChangeNeeded = false;
+        }
     }
 
     void TrainNewUnit()
@@ -46,5 +82,18 @@ public class TeamManager : MonoBehaviour
     void TrainNewSoldier()
     {
 
+    }
+
+    void SetAIMoveLocations()
+    {
+        //for (int i = 0; i < aiUnits.Length; i++)
+        //{
+        //    aiUnits[i].GetComponent<MoveAI>().movePosition = goldObject.transform.position;
+        //}
+    }
+
+    public void SetLocationReached()
+    {
+        locationChangeNeeded = true;
     }
 }
